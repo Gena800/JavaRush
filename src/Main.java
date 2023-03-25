659995c608d57348e38e6b0096fd86775ce1095e
Gennadii Lezhnev <genlezhnev@gmail.com>
2023-03-25 18:29:12 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import lvl_3.leson_6.Triangle;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle();
+        triangle.triangleTrue();
+
+    }
+}
